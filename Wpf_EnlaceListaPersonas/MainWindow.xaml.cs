b172183590fecaac815b315169f6b4870c09d0e0
zhuoIC <nicolimonar@gmail.com>
2017-05-17 12:56:54 +0200

    Terminado en clase

---
+++ Wpf_EnlaceListaPersonas/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//---------------------------
+using NHJ.Wpf_EnlacesDataContextAClases;
+
+namespace Wpf_EnlaceListaPersonas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListaPersonas _listaPersonas = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _listaPersonas = new ListaPersonas();
+            lbxListaPersonas.DataContext = _listaPersonas;
+        }
+
+        
+    }
+}
